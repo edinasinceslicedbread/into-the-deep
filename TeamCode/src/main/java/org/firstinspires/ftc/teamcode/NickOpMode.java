e0fb3dc0854cffdc0c783db633ea310ffe6da0a1
Nick Kleinjan <nickkleinjan@spacecrafting.com>
2024-09-21 09:47:42 -0500

    Update NickOpMode.java
    renamed class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NickOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NickOpMode.java
@@ -64,7 +64,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  */
 
 @TeleOp(name="Basic: Omni Linear OpMode", group="Linear OpMode")
-public class BasicOmniOpMode_Linear extends LinearOpMode {
+public class NickOpMode extends LinearOpMode {
 
     // Declare OpMode members for each of the 4 motors.
     private ElapsedTime runtime = new ElapsedTime();
