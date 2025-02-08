4793d40f1636881c9356b5c971ba58a47b3381b6
Nick Kleinjan <nickkleinjan@spacecrafting.com>
2025-02-07 18:20:24 -0600

    tested out color sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/claw/ClawColorSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/claw/ClawColorSensorTest.java
@@ -1,15 +1,14 @@
-package org.firstinspires.ftc.teamcode.testing;
+package org.firstinspires.ftc.teamcode.testing.claw;
 
 import android.annotation.SuppressLint;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ColorRangeSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-
-@TeleOp(name = "TEST | Claw | Servo Gripper Test", group = "$$$$")
-public class ClawServoGripperTest extends LinearOpMode {
+@TeleOp(name = "TEST | Claw | Color Sensor Test", group = "$$$$")
+public class ClawColorSensorTest extends LinearOpMode {
 
     // elapsed time
     private final ElapsedTime runtime = new ElapsedTime();
@@ -17,7 +16,7 @@ public class ClawServoGripperTest extends LinearOpMode {
     //------------------------------------------------------------------------------------------------
     // Hardware Definitions
     //------------------------------------------------------------------------------------------------
-    private Servo clawServo = null;
+    private ColorRangeSensor colorSensor;
 
     @SuppressLint("DefaultLocale")
     @Override
@@ -26,8 +25,8 @@ public class ClawServoGripperTest extends LinearOpMode {
         //------------------------------------------------------------------------------------------------
         // Hardware Setup
         //------------------------------------------------------------------------------------------------
-        clawServo = hardwareMap.get(Servo.class, "clawServo");
-        clawServo.setDirection(Servo.Direction.FORWARD);
+        colorSensor = hardwareMap.get(ColorRangeSensor.class, "colorSensor");
+
 
         //------------------------------------------------------------------------------------------------
         // Start Button
@@ -46,25 +45,41 @@ public class ClawServoGripperTest extends LinearOpMode {
         //------------------------------------------------------------------------------------------------
         while (opModeIsActive()) {
 
-            // left bumper opens
-            if (gamepad1.left_bumper || gamepad2.left_bumper)
-            {
-                clawServo.setPosition(0.30);
-            }
+            // color sensor data
+            int blue = colorSensor.blue();
+            int red = colorSensor.red();
+            int green = colorSensor.green();
 
-            // right bumper closes
-            if (gamepad1.right_bumper || gamepad2.right_bumper)
-            {
-                clawServo.setPosition(0.05);
+            int detectedColor = 0;
+            // blue is 1, red is 2, green is 3
+            // All number data for the if statement can be replaced with a different value for a set value depending on distance from block.
+            if (blue > red && blue > green && blue > 25) {
+                detectedColor = 1;
+            }
+            else if (red > green && red > blue && red > 40) {
+                detectedColor = 2;
+            }
+            else if (red > 60 && green > 50 && blue < 40) {
+                detectedColor = 4;
             }
 
             //------------------------------------------------------------------------------------------------
             // Telemetry Data
             //------------------------------------------------------------------------------------------------
             telemetry.addData("Run Time", runtime.toString());
-            telemetry.addLine("Press left / right bumpers to test the claw gripper.");
-            telemetry.addData("Claw Position", clawServo.getPosition());
+            telemetry.addLine("Hold a game piece under the sensor to test.");
+            telemetry.addLine("Red, green, and blue values are read by the sensor.");
+            telemetry.addLine("A color state of red, blue, green or yellow is determined.");
+            telemetry.addLine("--------------------------------------------------");
+            telemetry.addLine(String.format("Red=%d, Blue=%d, Green=%d", red, green, blue));
+            telemetry.addData("Detected Color State", detectedColor);
+            telemetry.addLine("--------------------------------------------------");
+            telemetry.addData("Red", detectedColor == 2);
+            telemetry.addData("Blue", detectedColor == 1);
+            telemetry.addData("Green", detectedColor == 3);
+            telemetry.addData("Yellow", detectedColor == 4);
             telemetry.update();
+
 
         }
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/claw/ClawServoGripperTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/claw/ClawServoGripperTest.java
@@ -1,20 +1,15 @@
-package org.firstinspires.ftc.teamcode.testing;
+package org.firstinspires.ftc.teamcode.testing.claw;
 
 import android.annotation.SuppressLint;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.ColorRangeSensor;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.TouchSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.teamcode.ctrl.RisingEdgeTrigger;
 
-
-@TeleOp(name = "TEST | Claw | Color Sensor Test", group = "$$$$")
-public class ClawColorSensorTest extends LinearOpMode {
+@TeleOp(name = "TEST | Claw | Servo Gripper Test", group = "$$$$")
+public class ClawServoGripperTest extends LinearOpMode {
 
     // elapsed time
     private final ElapsedTime runtime = new ElapsedTime();
@@ -22,7 +17,7 @@ public class ClawColorSensorTest extends LinearOpMode {
     //------------------------------------------------------------------------------------------------
     // Hardware Definitions
     //------------------------------------------------------------------------------------------------
-    private ColorRangeSensor colorSensor;
+    private Servo clawServo = null;
 
     @SuppressLint("DefaultLocale")
     @Override
@@ -31,8 +26,8 @@ public class ClawColorSensorTest extends LinearOpMode {
         //------------------------------------------------------------------------------------------------
         // Hardware Setup
         //------------------------------------------------------------------------------------------------
-        colorSensor = hardwareMap.get(ColorRangeSensor.class, "colorSensor");
-
+        clawServo = hardwareMap.get(Servo.class, "clawServo");
+        clawServo.setDirection(Servo.Direction.FORWARD);
 
         //------------------------------------------------------------------------------------------------
         // Start Button
@@ -51,41 +46,25 @@ public class ClawColorSensorTest extends LinearOpMode {
         //------------------------------------------------------------------------------------------------
         while (opModeIsActive()) {
 
-            // color sensor data
-            int blue = colorSensor.blue();
-            int red = colorSensor.red();
-            int green = colorSensor.green();
+            // left bumper opens
+            if (gamepad1.left_bumper || gamepad2.left_bumper)
+            {
+                clawServo.setPosition(0.30);
+            }
 
-            int detectedColor = 0;
-            // blue is 1, red is 2, green is 3
-            // All number data for the if statement can be replaced with a different value for a set value depending on distance from block.
-            if (blue > red && blue > green && blue > 25) {
-                detectedColor = 1;
-            }
-            if (red > green && red > blue && red > 40) {
-                detectedColor = 2;
-            }
-            if (red > 60 && green > 50 && blue < 40) {
-                detectedColor = 4;
+            // right bumper closes
+            if (gamepad1.right_bumper || gamepad2.right_bumper)
+            {
+                clawServo.setPosition(0.05);
             }
 
             //------------------------------------------------------------------------------------------------
             // Telemetry Data
             //------------------------------------------------------------------------------------------------
             telemetry.addData("Run Time", runtime.toString());
-            telemetry.addLine("Hold a game piece under the sensor to test.");
-            telemetry.addLine("Red, green, and blue values are read by the sensor.");
-            telemetry.addLine("A color state of red, blue, green or yellow is determined.");
-            telemetry.addLine("--------------------------------------------------");
-            telemetry.addLine(String.format("Red=%d, Blue=%d, Green=%d", red, green, blue));
-            telemetry.addData("Detected Color State", detectedColor);
-            telemetry.addLine("--------------------------------------------------");
-            telemetry.addData("Red", detectedColor == 2);
-            telemetry.addData("Blue", detectedColor == 1);
-            telemetry.addData("Green", detectedColor == 3);
-            telemetry.addData("Yellow", detectedColor == 4);
+            telemetry.addLine("Press left / right bumpers to test the claw gripper.");
+            telemetry.addData("Claw Position", clawServo.getPosition());
             telemetry.update();
-
 
         }
     }
